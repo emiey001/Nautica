55e9bca2615f3576608660b7d0f835fa41eb985b
emiey001 <93432004+emiey001@users.noreply.github.com>
2025-06-24 05:29:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "cdn.who.int.baalxei.eu.org"; // Ganti dengan domain utama kalian
-const serviceName = "nautica-dzo.pages.dev"; // Ganti dengan nama workers kalian
+const serviceName = "nautica"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
